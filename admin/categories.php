14ab2d39b47496863fa3368ff62580f257dcc830
Amir Sejdinovic <amir24V@yahoo.com>
2019-09-20 13:47:43 +0200

    Refactoring code
    Refactoring code into functions.

--- admin/categories.php
+++ admin/categories.php
@@ -24,37 +24,9 @@
                         <div class="col-xs-6">
                         <?php
 
-                         //provjera da li je forma submitana
-                         if(isset($_POST['submit'])){
-                           
-                           $cat_title = $_POST['cat_title'];
-                             //validacija forme
-                           if($cat_title == "" || empty($cat_title)){
-
-                            echo "This field shoudl not be empty";
-
-                           }else{
-                               //query za insert unosa u bazu podataka
-                            $query = "INSERT INTO categories(cat_title) ";
-                            $query .= "VALUE('{$cat_title}') ";
-
-                          
-                            //prosljeđivanje queryija u bazu
-                            $create_category_query = mysqli_query($connection, $query);
-
-                            //provjera konekcije  
-                            if(!$create_category_query){
-                              die("FAILED" . mysqli_error($connection));
-                            }
-                               
-
-                           }
-
-                         }
-
+                         insert_categories();
 
                         ?>
-
 
                         <form action="" method="post">
                         <label for="cat_title">Add Category</label>
@@ -72,6 +44,8 @@
                         </form>
 
                        <?php
+
+                       //Ažuriraj i uključi formu na klik na edit dugme
                        if(isset($_GET['edit'])){
                          $cat_id = $_GET['edit'];
 
@@ -99,15 +73,15 @@
 
                          <?php
                            //query za čupanje kategorija iz db
-                          $query = "SELECT * FROM categories";
+                           $query = "SELECT * FROM categories";
                        
-                          $select_categories_admin = mysqli_query($connection, $query);
-
-                         //while loop za prikaz kategorija iz db
-                         while($row = mysqli_fetch_assoc($select_categories_admin)){
-
-                            $cat_id = $row['cat_id'];
-                           $cat_title = $row['cat_title'];
+                           $select_categories_admin = mysqli_query($connection, $query);
+ 
+                          //while loop za prikaz kategorija iz db
+                          while($row = mysqli_fetch_assoc($select_categories_admin)){
+ 
+                             $cat_id = $row['cat_id'];
+                            $cat_title = $row['cat_title'];
                         ?>
                           <tr>
                           <td><?php echo $cat_id ?></td>
@@ -127,25 +101,8 @@
                          ?>
 
                          <?php
-
-                          //Provjeravamo da li je poslan get request sa parametrom "delete"
-                           if(isset($_GET['delete'])){
-                              
-
-                            //ako je poslan get sa gornjim parametrom onda čuvamo cat_id od tog parametra tj od id kategorije iz baze
-                              $delete_cat_id = $_GET['delete'];
-
-                              //nakon toga pravimo query sa zadatkom da izbriše kategoriju sa tim id iz baze podataka 
-                           $query = "DELETE FROM categories WHERE cat_id = {$delete_cat_id} ";
-                            //šaljemo query u bazu
-                           $delete_query_send = mysqli_query($connection, $query);
-
-                           //refrešujemo stranicu kako bi odmah na front pageu se vidjele promjene bez da user manuelno refrešuje
-                           header("Location: categories.php");
-
-
-
-                           }
+                         deleteCategories();
+                          
                          ?>
                          
                        
---
+++ admin/functions.php
@@ -0,0 +1,60 @@
+<?php
+function insert_categories(){
+   //uspostava globalne konekcije kako bi sve radilo kako treba
+  global $connection;
+
+//provjera da li je forma submitana
+if(isset($_POST['submit'])){
+                           
+  $cat_title = $_POST['cat_title'];
+    //validacija forme
+  if($cat_title == "" || empty($cat_title)){
+
+   echo "This field shoudl not be empty";
+
+  }else{
+      //query za insert unosa u bazu podataka
+   $query = "INSERT INTO categories(cat_title) ";
+   $query .= "VALUE('{$cat_title}') ";
+
+ 
+   //prosljeđivanje queryija u bazu
+   $create_category_query = mysqli_query($connection, $query);
+
+   //provjera konekcije  
+   if(!$create_category_query){
+     die("FAILED" . mysqli_error($connection));
+   }
+      
+
+  }
+
+}
+
+
+
+}
+
+function deleteCategories(){
+  global $connection;
+  //Provjeravamo da li je poslan get request sa parametrom "delete"
+  if(isset($_GET['delete'])){
+                              
+
+    //ako je poslan get sa gornjim parametrom onda čuvamo cat_id od tog parametra tj od id kategorije iz baze
+      $delete_cat_id = $_GET['delete'];
+
+      //nakon toga pravimo query sa zadatkom da izbriše kategoriju sa tim id iz baze podataka 
+   $query = "DELETE FROM categories WHERE cat_id = {$delete_cat_id} ";
+    //šaljemo query u bazu
+   $delete_query_send = mysqli_query($connection, $query);
+
+   //refrešujemo stranicu kako bi odmah na front pageu se vidjele promjene bez da user manuelno refrešuje
+   header("Location: categories.php");
+
+
+
+   }
+}
+
+?>

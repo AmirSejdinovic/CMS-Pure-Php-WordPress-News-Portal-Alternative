ba85bc780c0fbdd40f5ed155c672e828d97b804d
Amir Sejdinovic <amir24V@yahoo.com>
2019-09-23 10:43:04 +0200

    Admin Posts template HTML
    Template HTML for editing posts from admin panel

---
+++ admin/posts.php
@@ -0,0 +1,61 @@
+
+<?php include 'includes/header.php'; ?>
+<body>
+
+    <div id="wrapper">
+
+<?php include 'includes/nav.php'; ?>
+
+
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                    <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>
+                      <table class="table table-bordered table-hover">
+                        <thead>
+                          <tr>
+                            <th>ID</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Amir</td>
+                          <td>Super</td>
+                          <td>Amir</td>
+                          <td>fda</td>
+                          <td>fda</td>
+                          <td>sfd</td>
+                          <td>e3</td>
+                          <td>fda</td>
+                        </tr>
+                      </tbody>
+                      </table>
+                      
+                       
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+<?php include 'includes/footer.php'; ?>

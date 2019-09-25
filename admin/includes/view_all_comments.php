cfc574d67d4a372b5dbff99d789bc1b4fb4ff939
Amir Sejdinovic <amir24V@yahoo.com>
2019-09-25 12:00:36 +0200

    Inserting comments into database
    Here I build inserting comments from fron end form into database table

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -46,7 +46,7 @@
                              echo "<td>{$comment_content}</td>";
                              echo "<td>{$comment_email}</td>";
                              echo "<td>{$comment_status}</td>";
-                             echo "<td>{$comment_post_id}</td>";
+                             echo "<td>Some title</td>";
                              echo "<td>{$comment_date}</td>";
                              echo "<td><a href='posts.php?source=edit_post&p_id={$comment_id}'>Approve</a></td>";
                              echo "<td><a href='posts.php?delete={$comment_id}'>Unapprove</a></td>";

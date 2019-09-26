919f7a294f7dba454e05755b6ee9775cb15fa932
Amir Sejdinovic <amir24V@yahoo.com>
2019-09-26 15:24:00 +0200

    Update users
    Here I created query and funciontalty to update users from admin panel

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,84 +1,72 @@
-
 <?php
 
-if(isset($_GET['p_id'])){
-   
-    $post_id_current =  $_GET['p_id'];
+if(isset($_GET['edit_user'])){
+  $current_user_id = $_GET['edit_user'];
 
+  $query = "SELECT * FROM users WHERE user_id = $current_user_id ";
+  $edit_user_query = mysqli_query($connection, $query);
+  comfirm($edit_user_query);
+
+  while($row = mysqli_fetch_assoc($edit_user_query)){
+   $user_id = $row['user_id'];
+   $username = $row['username'];
+   $user_firstname = $row['user_firstname'];
+   $user_lastname = $row['user_lastname'];
+   $user_email = $row['user_email'];
+   $user_image = $row['user_image'];
+   $user_role = $row['user_role'];
+   $user_password = $row['user_password'];
+  }
 
 }
- $query ="SELECT * FROM posts WHERE post_id = $post_id_current";
- //uspostava konekcije i prosljeđivanje querya
- $select_posts_by_id = mysqli_query($connection, $query);
+  if(isset($_POST['edit_user'])){
 
- 
+           
+     $user_firstname = $_POST['user_firstname'];
+     $user_lastname = $_POST['user_lastname'];
+     $user_role = $_POST['user_role'];
 
- //While loop koja čupa sve iz baze iz tabele posts i pretvaranje pdataka u varjable
- while($row = mysqli_fetch_assoc($select_posts_by_id)){
+     $username = $_POST['username'];
 
-  $post_id = $row['post_id'];
-  $post_author = $row['post_author'];
-  $post_title = $row['post_title'];
-  $post_category_id = $row['post_category_id'];
-  $post_status = $row['post_status'];
-  $post_image = $row['post_image'];
-  $post_tags = $row['post_tags'];
-  $post_comment_count = $row['post_comment_count'];
-  $post_date = $row['post_date'];
-  $post_content = $row['post_content'];
+     
+     //$post_image = $_FILES['image']['name'];
+     //postavlja sliku u tmp fajl
+     //$post_image_temp = $_FILES['image']['tmp_name'];
 
- }
+     $user_email = $_POST['user_email'];
+     $user_password = $_POST['user_password'];
+     //$post_date = date('d-m-y');
+     //$post_comment_count = 0;
+     $user_image = "sp.jpg";//default value zahtjeva 
+      $randSalt = "sss";//default value zahtejva
 
- if(isset($_POST['update_post'])){
-    
-        $update_title = $_POST['post_title'];
-        $update_cat_id = $_POST['post_category_id'];
-        $update_post_author = $_POST['post_author'];
-        $update_post_title = $_POST['post_title'];
-        $update_post_status = $_POST['post_status'];
-        $update_image = $_FILES['post_image']['name'];
-        $update_image_temp = $_FILES['post_image']['tmp_name'];
-        $update_post_content =$_POST['post_content'];
-        $update_post_tags = $_POST['post_tags'];
+     //php funckija koja premjesta sliku iz temp fajl u lokaciju koju mi želimo to je ovdje folder images u root direktoriju
+     //move_uploaded_file($post_image_temp, "../images/$post_image");
 
+     $query ="UPDATE users SET ";
+     $query .="user_firstname = '{$user_firstname}', ";
+     $query .="user_lastname ='{$user_lastname}', ";
+     $query .="user_role = '{$user_role}', ";
+     $query .="username = '{$username}', ";
+     $query .="user_email = '{$user_email}', ";
+     $query .="user_password = '{$user_password}' ";
+     $query .="WHERE user_id = {$current_user_id} ";
+     
+     $update_user_query = mysqli_query($connection, $query);
 
-        move_uploaded_file($update_image_temp, "../images/$update_image");
-
-
-        if(empty($update_image)){
-           $query = "SELECT * FROM posts WHERE post_id = $post_id_current";
-           $query_select_image = mysqli_query($connection, $query);
-
-           comfirm($query_select_image);
-
-           while($row = mysqli_fetch_assoc($query_select_image)){
-                $post_image = $row['post_image'];
-           }
-        }
-
-        $query ="UPDATE posts SET ";
-        $query .="post_title = '{$update_post_title}', ";
-        $query .="post_category_id = {$update_cat_id}, ";
-        $query .="post_date = now(), ";
-        $query .="post_author = '{$update_post_author}', ";
-        $query .="post_status = '{$update_post_status}', ";
-        $query .="post_tags = '{$update_post_tags}', ";
-        $query .="post_content = '{$update_post_content}', ";
-        $query .="post_image = '{$update_image}' ";
-        $query .="WHERE post_id = {$post_id_current} ";
-       
-
-        
-
-        $update_post_query = mysqli_query($connection, $query);
-
-        comfirm($update_post_query);
+     comfirm($update_user_query );
 
 
 
 
- }
 
+
+
+
+
+
+
+  }
 
 ?>
 
@@ -87,94 +75,61 @@ if(isset($_GET['p_id'])){
 
 <div class="form-group">
 
- <label for="title">Post Title</label>
- <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
+ <label for="title">Firstname</label>
+ <input type="text" class="form-control" name="user_firstname" value="<?php echo $user_firstname; ?>">
+</div>
+
+<div class="form-group">
+
+ <label for="title">Lastname</label>
+ <input type="text" class="form-control" name="user_lastname" value="<?php echo $user_lastname; ?>">
 </div>
 
 
 
 <div class="form-group">
   <select name="user_role" id="">
-   <?php
-   
-   $query = "SELECT * FROM users";
-   $select_user_role = mysqli_query($connection, $query);
-
-   //comfirm($select_categories_by_id);
-
-   while($row = mysqli_fetch_assoc($select_user_role)){
-         $user_id = $row['user_id'];
-         $user_role = $row['user_role'];
-       
-
-         echo "<option value='{$user_id}'>$user_role</option>";
-
-
+  <option value="subscriber"><?php echo $user_role; ?></option>
+  <?php
+   if($user_role == 'admin'){
+    echo "<option value='subscriber'>Subsciber</option>";
+   }else{
+    echo "<option value='admin'>Admin</option>";
    }
-   
-   ?>
-  
-  
+  ?>
+     
+     
+     
   </select>
 
   <!--<label for="post_category">Post Category Id</label>
   <input type="text" class="form-control" name="post_category_id">-->
 </div>
 
+
+
+<!--<div class="form-group">
+  <label for="post_image">Post Image</label>
+  <input type="file" name="image">
+</div>-->
+
 <div class="form-group">
-  <select name="post_category_id" id="post_category">
-   <?php
-   
-   $query = "SELECT * FROM categories";
-   $select_categories_by_id = mysqli_query($connection, $query);
-
-   //comfirm($select_categories_by_id);
-
-   while($row = mysqli_fetch_assoc($select_categories_by_id)){
-         $cat_id = $row['cat_id'];
-         $cat_title = $row['cat_title'];
-
-         echo "<option value='{$cat_id}'>$cat_title</option>";
-
-
-   }
-   
-   ?>
-  
-  
-  </select>
+ <label for="post_tags">Username</label>
+ <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
 </div>
 
 <div class="form-group">
-  <label for="title">Post Author</label>
-  <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
+ <label for="post_content">Email</label>
+ <input type="email" class="form-control" name="user_email" value="<?php echo $user_email; ?>">
 </div>
 
 <div class="form-group">
-  <label for="post_status">Post Status</label>
-  <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
+ <label for="post_content">Password</label>
+ <input type="password" class="form-control" name="user_password" value="<?php echo  $user_password; ?>">
 </div>
 
 <div class="form-group">
-  <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
-  <label for="post_image">Chose Image</label>
-  <input type="file" name="post_image">
-</div>
-
-<div class="form-group">
- <label for="post_tags">Post Tags</label>
- <input value ="<?php echo $post_tags; ?>" type="text" class="form-control" name="post_tags">
-</div>
-
-<div class="form-group">
- <label for="post_content">Post Content</label>
- <textarea name="post_content" class="form-control" id="" cols="30" rows="10">
- <?php echo $post_content; ?> 
- </textarea>
-</div>
-
-<div class="form-group">
-  <input type="submit" class="btn btn-primary" name="update_post" value="Publish Post">
+  <input type="submit" class="btn btn-primary" name="edit_user" value="Add user">
 </div>
 
 

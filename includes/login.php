ecda8d62b849aa4225aeecc980f511f4cb129b67
Amir Sejdinovic <amir24V@yahoo.com>
2019-09-30 12:21:13 +0200

    User login front end
    Here I build funcitonality for user front end submit and redirect to admin page if the data is corect  if data is not corect then redirect to index.php

--- includes/login.php
+++ includes/login.php
@@ -22,9 +22,23 @@ if(isset($_POST['login'])){
 
   //while loop za čupanje podataka o useru
 while($row = mysqli_fetch_assoc($select_user_query)){
-  echo $db_id = $row['user_id'];
-  $username = $row['username'];
-  $password = $row['user_password'];
+  $db_id = $row['user_id'];
+  $db_username = $row['username'];
+  $db_password = $row['user_password'];
+  $db_firstname = $row['user_firstname'];
+  $db_lastname = $row['user_lastname'];
+  $db_user_role = $row['user_role'];
+
+}
+
+//If statement koja provjerava uneseni username i pasword sa podacima iz baze podataka ako se ti podaci ne podudaraju onda redirektuje na index.php ako se podudaraju onda redirektuje na admin sekciju ako se bilo šta mimo toga desi npr pogodi ime ali ne pogodi pasword onda opet redirektuje na index.php
+if($username !== $db_username && $password !== $db_password){
+    
+  header("Location: ../index.php");
+}else if($username == $db_username && $password == $db_password){
+   header("Location: ../admin");
+}else{
+  header("Location: ../index.php");
 }
 
 }

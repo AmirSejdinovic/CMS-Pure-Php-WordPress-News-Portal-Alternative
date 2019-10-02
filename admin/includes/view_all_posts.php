676d702b4eaff532ff693aa4930ce1332a9106bb
Amir Sejdinovic <amir24V@yahoo.com>
2019-10-02 15:19:54 +0200

    Adding bulk options to view all post page

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,6 +1,37 @@
+<?php
+
+if(isset($_POST['checkBoxArray'])){
+  
+  foreach($_POST['checkBoxArray'] as $checkBoxValue){
+      
+    $bulk_options = $_POST['bulk_options'];
+  }
+}
+
+?>
+
+
+<form action="" method="post">
 <table class="table table-bordered table-hover">
+
+
+          <div id="bulkOptionsContainer" class="col-xs-4">
+            <select  class="form-control" name="bulk_options" id="">
+              <option value="">Select Options</option>
+              <option value="">Publish</option>
+              <option value="">Draft</option>
+              <option value="">Delete</option>
+            
+            </select>
+          </div>
+
+          <div class="col-xs-4">
+           <input type="submit" name="submit" class="btn btn-success" value="Apply">
+           <a class="btn btn-primary" href="add_post.php">Add New</a>
+          </div>
                         <thead>
                           <tr>
+                            <th><input id="selectAllBoxes" type="checkbox"></th>
                             <th>ID</th>
                             <th>Author</th>
                             <th>Title</th>
@@ -43,6 +74,13 @@
 
 
                            echo "<tr>";
+
+                           ?>
+
+                             <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'/></td>
+
+
+                             <?php
                              echo "<td>{$post_id}</td>";
                              echo "<td>{$post_author}</td>";
                              echo "<td>{$post_title}</td>";
@@ -81,6 +119,7 @@
                           
                       </tbody>
                       </table>
+                      </form>
 
 
                       <?php

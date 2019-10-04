d8398f923a5b84e60cc52ed879bb5ce4667ccfc5
Amir Sejdinovic <amir24V@yahoo.com>
2019-10-04 11:26:22 +0200

    Adding related posts page and query

--- author_posts.php
+++ author_posts.php
@@ -16,14 +16,18 @@
             <!-- Blog Entries Column -->
     <div class="col-md-8">
 
-    <h1 class="page-header">
-    Page Heading
-    <small>Secondary Text</small>
-</h1>
 
      <?php
+
+
+       if(isset($_GET['p_id'])){
+              
+          $current_post_id =  $_GET['p_id'];
+          $the_post_author = $_GET['author'];
+
+       }
      //Izaberi sve iz post tabele
-     $query = "SELECT * FROM posts ";
+     $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' ";
       //uspostava konekcije sa bazom i prosljeđivanje queriya
      $select_all_posts = mysqli_query($connection, $query);
 
@@ -34,57 +38,53 @@
 
      while($row = mysqli_fetch_assoc($select_all_posts)){
 
-         //čupanje id iz rowa
-         $post_id = $row['post_id'];
          $post_title = $row['post_title'];//čupanje post_title rowa
          $post_author = $row['post_author'];//čupanje post_author rowa iz tablee
          $post_date = $row['post_date'];//čupanje post_date rowa iz tabele
          $post_image = $row['post_image'];//čupanje post_image rowa iz tabele
-         $post_content = substr($row['post_content'],0,150);//čupanje post_contene rowa iz rabele, pravljenje putem fije substr excerpta od 0 do 150 karaktera
-          $post_status = $row['post_status'];
+         $post_content = $row['post_content'];//čupanje post_contene rowa iz rabele
 
-          if($post_status == 'published'){
-
-
-
-
-
-          
          //prekid while loop
 
          ?>
 
 
-
+<h1 class="page-header">
+    Page Heading
+    <small>Secondary Text</small>
+</h1>
 
 <!-- First Blog Post -->
 <h2>
-    <a href="post.php?p_id=<?php //postavljanje getera sa post id
-    echo $post_id; ?>"><?php echo $post_title; //display title ?></a>
+    <a href="#"><?php echo $post_title; //display title ?></a>
 </h2>
 <p class="lead">
-    by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
+   All posts by: <?php echo $post_author; ?></a>
 </p>
 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
 <hr>
-<a href="post.php?p_id=<?php echo $post_id; ?>">
 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-</a>
 <hr>
 <p><?php echo $post_content; ?></p>
-<a class="btn btn-primary" href="post.php?p_id=<?php //postavljanje getera sa post id
-    echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
 
 <hr>
 
 
 
 <?php
-          }
+
 //nastavak whille loop
 
      }//kraj whille loop
    ?>
+
+
+   
+
+             
+                
+                
 
    </div>
 <?php include 'includes/sidebar.php' ?>
